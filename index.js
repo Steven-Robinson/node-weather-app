02a28c3ee3f0e84b66a4cc5f32182257f0d96438
Steven Robinson <steverobinson1978@gmail.com>
2016-01-12 21:58:52 +0000

    Fixes.

--- index.js
+++ index.js
@@ -14,31 +14,33 @@ var request = require('request'),
 
 	url = (function (url, params) {
 		qs = querystring.stringify(params);
-		return 'http://' + url + '?' + qs;
+		return 'http://' + url.replace('http://', '') + '?' + qs;
 	}(service, config)),
 
 	mybody;
+
+if (process.argv[2] == 'reset') {
+	db.run("DROP table weather_data");
+}
 
 request(url, function (err, res, body) {
 	if (!err && res.statusCode == 200) {
 		mybody = body;
 	}
-});
 
-db.serialize(function() {
-	if (process.argv[2] == 'reset') {
-		db.run("DROP table weather_data");
-	}
+	db.serialize(function() {
+		db.run("CREATE TABLE if not exists weather_data (data TEXT)");
+		
+		var stmt = db.prepare("INSERT INTO weather_data VALUES (?)");
+		
+		stmt.run(mybody);
+		stmt.finalize();
 
-	db.run("CREATE TABLE if not exists weather_data (data TEXT)");
-	
-	var stmt = db.prepare("INSERT INTO weather_data (data) VALUES (?)");
-	stmt.run();
-	stmt.finalize();
-
-	db.each("SELECT rowid AS id, data FROM weather_data", function(err, row) {
-		console.log(row.id + ": " + row.data);
+		db.each("SELECT rowid AS id, data FROM weather_data", function(err, row) {
+			console.log(row.id + ": " + row.data);
+		});
 	});
+
+	db.close();
 });
 
-db.close();
